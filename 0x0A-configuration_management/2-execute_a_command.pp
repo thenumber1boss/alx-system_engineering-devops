808b5d2963ebe9163bf49cc8cba73474b9f3f150
thenumber1boss <bamijokoseun@gmail.com>
2023-07-19 13:24:11 -0700

    pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,10 +1,7 @@
 # create a manifest that kills a process named killmenow
 # using pkill with puppet
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin:/bin',  # Set appropriate paths for pkill
-  onlyif      => 'pgrep -f killmenow',  # Check if the process exists before attempting to kill it
-  refreshonly => true,  # Ensure the command only runs when explicitly refreshed
-  logoutput   => true,  # Log the output of the command for troubleshooting
-}
 
+exec { 'pkill':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin',
+}
